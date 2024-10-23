375dd2fa18a50dafb3b76ba32ffbf7e1ca788916
Piero010405 <84740254+Piero010405@users.noreply.github.com>
2024-10-23 17:49:52 -0500

    tests si no compilas fue

--- tests/validation_test.cpp
+++ tests/validation_test.cpp
@@ -2,7 +2,16 @@
 
 #include <random>
 
+#include "secuencial1.h"
 #include "sequential.h"
+#include "estandar.h"
+#include "estandar_reduction.h"
+#include "openmp_atomic.h"
+#include "openmp_reduction.h"
+#include "openmp_lock_guard.h"
+#include "openmp_lock_unlock.h"
+#include "openmp_critical.h"
+#include "openmp_ompatomic.h"
 
 static int* randomInput = nullptr;
 static const int MAXIMO_VALOR = 5;
@@ -29,7 +38,97 @@ void finaliza() {
 }
 
 TEST(SequentialTest, pruebaOK) {
+  Sequential1 histogramCalculator;
+  auto histograma = histogramCalculator.calculate(randomInput, MAXIMO_VALOR,
+                                                  NUMERO_ELEMENTOS);
+
+  int acum = 0;
+  for(auto puntuacion : histograma) acum += puntuacion;
+  EXPECT_EQ(acum, NUMERO_ELEMENTOS);
+}
+
+TEST(Sequential2Test, pruebaOK) {
   Sequential histogramCalculator;
+  auto histograma = histogramCalculator.calculate(randomInput, MAXIMO_VALOR,
+                                                  NUMERO_ELEMENTOS);
+
+  int acum = 0;
+  for(auto puntuacion : histograma) acum += puntuacion;
+  EXPECT_EQ(acum, NUMERO_ELEMENTOS);
+}
+
+TEST(EstandarTest, pruebaOK) {
+  Estandar histogramCalculator;
+  auto histograma = histogramCalculator.calculate(randomInput, MAXIMO_VALOR,
+                                                  NUMERO_ELEMENTOS);
+
+  int acum = 0;
+  for(auto puntuacion : histograma) acum += puntuacion;
+  EXPECT_EQ(acum, NUMERO_ELEMENTOS);
+}
+
+TEST(EstandarReductionTest, pruebaOK) {
+  EstandarReduction histogramCalculator;
+  auto histograma = histogramCalculator.calculate(randomInput, MAXIMO_VALOR,
+                                                  NUMERO_ELEMENTOS);
+
+  int acum = 0;
+  for(auto puntuacion : histograma) acum += puntuacion;
+  EXPECT_EQ(acum, NUMERO_ELEMENTOS);
+}
+
+TEST(OpenMPAtomicTest, pruebaOK) {
+  OpenMPAtomic histogramCalculator;
+  auto histograma = histogramCalculator.calculate(randomInput, MAXIMO_VALOR,
+                                                  NUMERO_ELEMENTOS);
+
+  int acum = 0;
+  for(auto puntuacion : histograma) acum += puntuacion;
+  EXPECT_EQ(acum, NUMERO_ELEMENTOS);
+}
+
+TEST(OpenMPReductionTest, pruebaOK) {
+  OpenMPReduction histogramCalculator;
+  auto histograma = histogramCalculator.calculate(randomInput, MAXIMO_VALOR,
+                                                  NUMERO_ELEMENTOS);
+
+  int acum = 0;
+  for(auto puntuacion : histograma) acum += puntuacion;
+  EXPECT_EQ(acum, NUMERO_ELEMENTOS);
+}
+
+TEST(OpenMPLockGuardTest, pruebaOK) {
+  OpenMPLockGuard histogramCalculator;
+  auto histograma = histogramCalculator.calculate(randomInput, MAXIMO_VALOR,
+                                                  NUMERO_ELEMENTOS);
+
+  int acum = 0;
+  for(auto puntuacion : histograma) acum += puntuacion;
+  EXPECT_EQ(acum, NUMERO_ELEMENTOS);
+}
+
+TEST(OpenMPLockUnlockTest, pruebaOK) {
+  OpenMPLockUnlock histogramCalculator;
+  auto histograma = histogramCalculator.calculate(randomInput, MAXIMO_VALOR,
+                                                  NUMERO_ELEMENTOS);
+
+  int acum = 0;
+  for(auto puntuacion : histograma) acum += puntuacion;
+  EXPECT_EQ(acum, NUMERO_ELEMENTOS);
+}
+
+TEST(OpenMPCriticalTest, pruebaOK) {
+  OpenMPCritical histogramCalculator;
+  auto histograma = histogramCalculator.calculate(randomInput, MAXIMO_VALOR,
+                                                  NUMERO_ELEMENTOS);
+
+  int acum = 0;
+  for(auto puntuacion : histograma) acum += puntuacion;
+  EXPECT_EQ(acum, NUMERO_ELEMENTOS);
+}
+
+TEST(OpenMPOMPAtomicTest, pruebaOK) {
+  OpenMPOMPAtomic histogramCalculator;
   auto histograma = histogramCalculator.calculate(randomInput, MAXIMO_VALOR,
                                                   NUMERO_ELEMENTOS);
 
