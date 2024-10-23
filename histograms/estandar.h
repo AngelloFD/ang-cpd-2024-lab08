d2b8f52012eb45c3adaebaa2a480762bba7d909a
Piero010405 <84740254+Piero010405@users.noreply.github.com>
2024-10-23 15:50:31 -0500

    static void

--- histograms/estandar.h
+++ histograms/estandar.h
@@ -7,7 +7,7 @@
 class Estandar : public Histogram {
  public:
   std::vector<int> calculate(const int*, const int, const int) override;
-  void calcular_histograma(int* histograma, int inicio, int fin, const int* input, std::mutex mtx);
+  static void calcular_histograma(int* histograma, int inicio, int fin, const int* input, std::mutex mtx);
 };
 
 #endif
