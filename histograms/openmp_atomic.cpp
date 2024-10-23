742de3cd92fa4f5a62292e57f0690a1b0697e48b
Piero010405 <84740254+Piero010405@users.noreply.github.com>
2024-10-23 15:46:43 -0500

    fixed atomic

--- histograms/openmp_atomic.cpp
+++ histograms/openmp_atomic.cpp
@@ -2,7 +2,11 @@
 #include <atomic>
 
 std::vector<int> OpenMPAtomic::calculate(const int* input, const int buckets, const int input_size) {
-    std::vector<std::atomic<int>> histogram(buckets, 0);  
+    std::vector<std::atomic<int>> histogram(buckets);
+    for (int i = 0; i < buckets; i++) {
+        histogram[i].store(0); 
+    } 
+
     #pragma omp parallel for
     for(int idx = 0; idx < input_size; idx++) {
       histogram[input[idx] - 1]++;
